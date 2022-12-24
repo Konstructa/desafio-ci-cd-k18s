7c77c80b224b0f032824995c0b54a871e64e588b
Konstructa <elenprojects7@gmail.com>
2022-12-23 22:10:16 -0300

    FEAT: create script

--- script.sh
+++ script.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-echo "Criando imagens..."
+kubectl apply -f .\mysql-deployment.yml --record
 
-docker build -t konstructa/backend-php:1.0 backend/.
-
-docker build -t konstructa/database-php:1.0 database/.
-
-echo "Realizando push das imagens..."
-
-docker push konstructa/backend-php:1.0
-
-docker push konstructa/database-php:1.0
-
-echo "Criando serviços no cluster kubernetes..."
-
-kubctl apply -f ./services.yml
-
-echo "Criando os deployments..."
-
-kubctl apply -f ./deployment.yml
+kubectl apply -f .\app-deployment.yml --record
